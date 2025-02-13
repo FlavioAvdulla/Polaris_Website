161704f1a83f6ddd169e476902c53225dc04106a
Flavio Avdulla <a.flavio4366@gmail.com>
2025-02-13 12:19:11 +0100

    web_update

--- FrontEnd/tailwind.config.js
+++ FrontEnd/tailwind.config.js
@@ -45,7 +45,7 @@ module.exports = {
       },
       screens: {
         'xs': '425px',
-        'sm': '640px',
+        'sm': '400px',
         'md': '768px',
         'lg': '1024px',
         'xl': '1440px',
