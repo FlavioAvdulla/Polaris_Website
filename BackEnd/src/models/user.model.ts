f76a047aad1f25afe4c71fa851c3c652117fa09a
Flavio Avdulla <a.flavio4366@gmail.com>
2025-03-11 13:50:11 +0100

    web_update

--- BackEnd/src/models/user.model.ts
+++ BackEnd/src/models/user.model.ts
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import { hashValue } from "../utils/bcrypt";
 
 export interface UserDocument extends mongoose.Document {
   email: string;
@@ -9,7 +10,25 @@ export interface UserDocument extends mongoose.Document {
   comparePassword(val: string): Promise<boolean>;
 }
 
-const userSchema = new mongoose.Schema<UserDocument>({
-    email: { type: String, unique: true, required: true},
-    
-})
+const userSchema = new mongoose.Schema<UserDocument>(
+  {
+    email: { type: String, unique: true, required: true },
+    password: { type: String, reuqired: true },
+    verified: { type: Boolean, required: true, default: false },
+  },
+  {
+    timestamp: true,
+  }
+);
+
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) {
+        return next()
+    }
+  this.password = await hashValue(this.password)
+  next();
+});
+
+userSchema.methods.comparePassword = async function(val: string) {
+    return compareValue(val, this.password)
+}
