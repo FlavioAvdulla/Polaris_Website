dc93a7e2077986e189e37bed99e3e3a963b9fc7b
Flavio Avdulla <a.flavio4366@gmail.com>
2025-03-11 22:32:59 +0100

    web_update

--- BackEnd/src/services/auth.service.ts
+++ BackEnd/src/services/auth.service.ts
@@ -1,35 +1,57 @@
+import { JWT_REFRESH_SECRET } from "../constants/env";
 import VerificationCodeType from "../constants/verificationCodeTypes";
+import SessionModel from "../models/session.model";
 import UserModel from "../models/user.model";
 import VerificationCodeModel from "../models/verificationCode.model";
 import { oneYearFromNow } from "../utils/date";
 
 export type createAccountParams = {
-    email: string;
-    password: string;
-    userAgent?: string;
-}
+  email: string;
+  password: string;
+  userAgent?: string;
+};
 
-export const createAccount = async (data:createAccountParams) => {
-    // Verify existing user doesnt exist
-    const existingUser = await UserModel.exists({
-        email:data.email
-    })
-    if (existingUser) {
-        throw new Error("User already exists.")
+export const createAccount = async (data: createAccountParams) => {
+  // Verify existing user doesnt exist
+  const existingUser = await UserModel.exists({
+    email: data.email,
+  });
+  if (existingUser) {
+    throw new Error("User already exists.");
+  }
+  // Creat user
+  const user = await UserModel.create({
+    email: data.email,
+    password: data.password,
+  });
+  // create verification code
+  const verificationCode = await VerificationCodeModel.create({
+    userId: user._id,
+    type: VerificationCodeType.EmailVerification,
+    expiresAt: oneYearFromNow(),
+  });
+  // send verification email
+  // create session
+  const session = await SessionModel.create({
+    userId: user._id,
+    userAgent: data.userAgent,
+  });
+  // sign access token & refresh token
+  const refreshToken = jwt.sign(
+    { sessionId: session._id },
+    JWT_REFRESH_SECRET,
+    {
+      audience: ["user"],
+      expiresIn: "30d",
     }
-    // Creat user
-    const user = await UserModel.create({
-        email: data.email,
-        password: data.password,
-    })
-    // create verification code
-    const verificationCode = await VerificationCodeModel.create({
-        userId: user._id,
-        type: VerificationCodeType.EmailVerification,
-        expiresAt: oneYearFromNow()
-    })
-    // send verification email
-    // create session
-    // sign access token & refresh token
-    // return user
-}
+  );
+  const accessToken = jwt.sign(
+    { userId: user._id, sessionId: session._id },
+    JWT_SECRET,
+    {
+      audience: ["user"],
+      expiresIn: "15m",
+    }
+  );
+  // return user
+};
