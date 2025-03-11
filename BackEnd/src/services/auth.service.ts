9750a6adf6488d70a2479fc9e20b88fcd295b520
Flavio Avdulla <a.flavio4366@gmail.com>
2025-03-11 21:47:50 +0100

    web_update

--- BackEnd/src/services/auth.service.ts
+++ BackEnd/src/services/auth.service.ts
@@ -1,3 +1,8 @@
+import VerificationCodeType from "../constants/verificationCodeTypes";
+import UserModel from "../models/user.model";
+import VerificationCodeModel from "../models/verificationCode.model";
+import { oneYearFromNow } from "../utils/date";
+
 export type createAccountParams = {
     email: string;
     password: string;
@@ -6,8 +11,23 @@ export type createAccountParams = {
 
 export const createAccount = async (data:createAccountParams) => {
     // Verify existing user doesnt exist
+    const existingUser = await UserModel.exists({
+        email:data.email
+    })
+    if (existingUser) {
+        throw new Error("User already exists.")
+    }
     // Creat user
+    const user = await UserModel.create({
+        email: data.email,
+        password: data.password,
+    })
     // create verification code
+    const verificationCode = await VerificationCodeModel.create({
+        userId: user._id,
+        type: VerificationCodeType.EmailVerification,
+        expiresAt: oneYearFromNow()
+    })
     // send verification email
     // create session
     // sign access token & refresh token
