c33a7f69e48cea1352769f9302bb5257f2b30288
Flavio Avdulla <a.flavio4366@gmail.com>
2025-03-16 21:42:25 +0100

    web_update

--- BackEnd/src/controllers/auth.controller.ts
+++ BackEnd/src/controllers/auth.controller.ts
@@ -3,8 +3,10 @@ import { Request, Response, NextFunction } from "express";
 import catchErrors from "../utils/catchErrors";
 import { createAccount, loginUser } from "../services/auth.service";
 import { CREATED, OK } from "../constants/http";
-import { setAuthCookies } from "../utils/cookies";
+import { clearAuthCookies, setAuthCookies } from "../utils/cookies";
 import { loginSchema, registerSchema } from "./auth.schemas";
+import { AccessTokenPayload, verifyToken } from "../utils/jwt";
+import SessionModel from "../models/session.model";
 
 export const registerHandler = catchErrors(
   async (req: Request, res: Response) => {
@@ -36,6 +38,20 @@ export const loginHandler = catchErrors(async (req, res) => {
   const { accessToken, refreshToken } = await loginUser(request);
 
   return setAuthCookies({ res, accessToken, refreshToken }).status(OK).json({
-    message: "Login successful",
+    message: "Login successful.",
+  });
+});
+
+export const logoutHandler = catchErrors(async (req, res) => {
+  const accessToken = req.cookies.accessToken;
+  const { payload } = verifyToken(accessToken);
+
+  if (payload) {
+    await SessionModel.findByIdAndDelete(payload.sessionId);
+  }
+
+  return clearAuthCookies(res).
+  status(OK).json({
+    message: "Logout successful.",
   });
 });
--- BackEnd/src/utils/cookies.ts
+++ BackEnd/src/utils/cookies.ts
@@ -1,6 +1,7 @@
 import { CookieOptions, Response } from "express";
 import { fifteenMinutesFromNow, thirtyDaysFromNow } from "./date";
 
+export const REFRESH_PATH = "/auth/refresh"
 const secure = process.env.NODE_ENV !== "development";
 
 const defaults: CookieOptions = {
@@ -12,13 +13,13 @@ const defaults: CookieOptions = {
 const getAccessTokenCookieOptions = (): CookieOptions => ({
   ...defaults,
   expires: fifteenMinutesFromNow(),
-  path: "/auth/access"
+  path: "/auth/access",
 });
 
 const getRefreshTokenCookieOptions = (): CookieOptions => ({
   ...defaults,
   expires: thirtyDaysFromNow(),
-  path: "/auth/refresh",
+  path: "REFRESH_PATH",
 });
 
 type Params = {
@@ -31,3 +32,8 @@ export const setAuthCookies = ({ res, accessToken, refreshToken }: Params) =>
   res
     .cookie("accessToken", accessToken, getAccessTokenCookieOptions())
     .cookie("refreshToken", refreshToken, getRefreshTokenCookieOptions());
+
+export const clearAuthCookies = (res: Response) =>
+  res.clearCookie("accessToken").clearCookie("refreshToken", {
+    path: "REFRESH_PATH",
+  });
