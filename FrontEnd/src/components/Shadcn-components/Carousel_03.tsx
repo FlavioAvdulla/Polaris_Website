a9339765f8e383197c997639c18157fb2d12f89f
Flavio Avdulla <a.flavio4366@gmail.com>
2025-04-29 21:51:54 +0200

    web_update

--- FrontEnd/src/components/Shadcn-components/Carousel_03.tsx
+++ FrontEnd/src/components/Shadcn-components/Carousel_03.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 
 import { Card, CardContent } from "@/components/ui/card";
-import { carousel_03 } from "../Home/ProductSection/ProductSection";
+import { carousel_04 } from "../Home/ProductSection/ProductSection";
 import {
   Carousel,
   CarouselContent,
@@ -44,7 +44,7 @@ export function Carousel_03() {
       } else {
         api.scrollNext();
       }
-    }, 7000);
+    }, 10000);
 
     return () => clearInterval(interval);
   }, [api, count]);
@@ -58,16 +58,16 @@ export function Carousel_03() {
         onMouseUp={() => setIsGrabbing(false)}
       >
         <CarouselContent>
-          {carousel_03.map((product, index) => (
+          {carousel_04.map((product, index) => (
             <CarouselItem key={index}>
               <Card className="border-none shadow-none">
                 <CardContent
                   className={`flex items-center m-0 p-0 rounded-xl overflow-hidden
-                                                    ${
-                                                      isGrabbing
-                                                        ? "cursor-grabbing"
-                                                        : "cursor-grab"
-                                                    }`}>
+                              ${
+                                isGrabbing
+                                  ? "cursor-grabbing"
+                                  : "cursor-grab"
+                              }`}>
                   <div className="flex items-center w-[100%]
                                   
                                   xl:h-[700px]">
@@ -76,7 +76,7 @@ export function Carousel_03() {
                                                     
                                 xs:w-[100%] xs:pl-7 xs:bottom-0 xs:mb-[30px]
                                 sm:w-[80%] sm:bottom-0 sm:mb-[30px]
-                                md:w-[80%] md:mb-[70px] md:gap-5
+                                md:w-[80%] md:bottom-0 md:mb-[70px] md:gap-5
                                 lg:w-[90%] lg:mb-[30px] lg:gap-2
                                 xl:w-[90%] xl:pl-12
                                 2xl:bottom-0 2xl:mb-[30px] 2xl:gap-0">
@@ -85,24 +85,22 @@ export function Carousel_03() {
                         className="gap-3
                                                       
                                   xs:flex">
-                        <p
-                          className="flex text-white gap-3 items-center
+                        <p className="flex text-white gap-3 items-center
                                                                       
-                                    xs:text-[10px]
-                                    sm:text-[13px]
-                                    md:text-[20px]
-                                    lg:text-[15px]
-                                    2xl:text-[16px]">
+                                      xs:text-[11px]
+                                      sm:text-[13px]
+                                      md:text-[20px]
+                                      lg:text-[15px]
+                                      2xl:text-[16px]">
                           {t(product.exclusiveOffer)}
                         </p>
                         <div
                           className="w-fit rounded-br-3xl rounded-tr-3xl rounded-tl-lg rounded-bl-lg bg-primary border-[1px] cursor-pointer
                                     border-white text-white items-center justify-center
                                       hover:bg-transparent hover:scale-110 ease-in-out duration-300">
-                          <p
-                            className="
+                          <p className="
                                                                               
-                                      xs:text-[10px] xs:px-4 xs:py-[1px]
+                                      xs:px-4 xs:text-[15px] xs:py-[1px]
                                       md:text-[20px] md:px-6 md:py-[3px]
                                       lg:text-[15px]
                                       2xl:text-[16px]">
@@ -110,36 +108,34 @@ export function Carousel_03() {
                           </p>
                         </div>
                       </div>
-                      <h1
-                        className="text-white font-camptonBold leading-tight my-3
+                      <h1 className="text-white font-camptonBold leading-tight my-3
                                                       
-                                  xs:text-[22px] xs:w-[80%]
-                                  sm:text-[30px]
-                                  md:text-[65px]
-                                  lg:text-[35px]
-                                  2xl:text-[45px]">
+                                    xs:text-[30px] xs:w-[80%]
+                                    md:text-[65px]
+                                    lg:text-[35px]
+                                    2xl:text-[45px]">
                         {t(product.title)}
                       </h1>
-                      <p
-                        className="text-white font-camptonBook
+                      <p className="text-white font-camptonBook
                                                       
-                                  xs:text-[8px] xs:w-[80%]
-                                  sm:text-[13px] sm:w-[90%]
-                                  md:w-[100%] md:text-[20px]
-                                  lg:text-[15px] lg:w-[90%]
-                                  2xl:text-[16px] 2xl:w-[100%]">
+                                    xs:text-[12px] xs:w-[80%]
+                                    sm:text-[13px] sm:w-[90%]
+                                    md:w-[100%] md:text-[20px]
+                                    lg:text-[15px] lg:w-[90%]
+                                    2xl:text-[16px] 2xl:w-[100%]">
                         {t(product.paragraph)}
                       </p>
                       <div className="w-auto mt-7">
                         <button
-                          className="flex items-center justify-center bg-primary border-[1px] border-white text-white rounded-br-3xl rounded-tr-3xl rounded-tl-lg rounded-bl-lg
+                          className="flex items-center justify-center bg-primary border-[1px] border-white
+                                  text-white rounded-br-3xl rounded-tr-3xl rounded-tl-lg rounded-bl-lg
                                     hover:scale-110 hover:border-[1px] hover:bg-transparent ease-in-out duration-300
                                                                           
-                                      xs:gap-2 xs:px-3 xs:py-1
-                                      md:gap-3 md:px-4 md:py-2">
-                          <p
-                            className="
-                                      xs:text-[10px]
+                                    xs:gap-2 xs:px-3 xs:py-1
+                                    md:gap-3 md:px-4 md:py-2">
+                          <p className="
+                                     
+                                      xs:text-[12px]
                                       md:text-[20px]
                                       lg:text-[15px]
                                       2xl:text-[20px]">
@@ -148,11 +144,11 @@ export function Carousel_03() {
                           <i>
                             <IoIosArrowForward
                               className="
-                                        xs:text-[10px]
+                                        
+                                        xs:text-[12px]
                                         md:text-[15px]
                                         lg:text-[20px]
-                                        2xl:text-[20px]"
-                            />
+                                        2xl:text-[20px]"/>
                           </i>
                         </button>
                       </div>
