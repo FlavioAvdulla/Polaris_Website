abf67d9f66276d46638ac27165269e38f3be4cb9
Flavio Avdulla <a.flavio4366@gmail.com>
2025-04-29 21:57:51 +0200

    web_update

--- FrontEnd/src/components/Shadcn-components/Carousel_05.tsx
+++ FrontEnd/src/components/Shadcn-components/Carousel_05.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 
 import { Card, CardContent } from "@/components/ui/card";
-import { carousel_05 } from "../Home/ProductSection/ProductSection";
+import { carousel_04 } from "../Home/ProductSection/ProductSection";
 import {
   Carousel,
   CarouselContent,
@@ -44,97 +44,111 @@ export function Carousel_05() {
       } else {
         api.scrollNext();
       }
-    }, 7000);
+    }, 10000);
 
     return () => clearInterval(interval);
   }, [api, count]);
 
   return (
-    <div
-      className="mx-auto
-    
-                    xs:w-[100%]
-                    lg:w-[50%]">
+    <div className="mx-auto w-[100%]">
       <Carousel
         setApi={setApi}
         className="w-full flex flex-col"
         onMouseDown={() => setIsGrabbing(true)}
-        onMouseUp={() => setIsGrabbing(false)}>
+        onMouseUp={() => setIsGrabbing(false)}
+      >
         <CarouselContent>
-          {carousel_05.map((product, index) => (
+          {carousel_04.map((product, index) => (
             <CarouselItem key={index}>
               <Card className="border-none shadow-none">
                 <CardContent
-                  className={`flex aspect-square items-center justify-center m-0 p-0
-
-                              xs:h-auto
-                              lg:h-[600px]
-                              xl:h-[700px]
-
-                    rounded-xl overflow-hidden bg-slate-300 ${
-                      isGrabbing ? "cursor-grabbing" : "cursor-grab"
-                    }`}>
-                  <div className="flex items-center w-[100%] h-[100%]">
-                    <div className="flex flex-col absolute w-[100%] h-auto bottom-0 justify-center
-                    
-                                    
-                                    xs:p-5
-                                    sm:p-7
-                                    md:p-12 md:mb-5
-                                    lg:p-6 lg:mb-0
-                                    xl:mb-5">
-                      <p className="flex text-white gap-3 items-center
-                      
-                                    xs:text-[12px]
-                                    md:text-[20px]
-                                    lg:text-[12px]
-                                    xl:text-[15px]">
-                        {t(product.exclusiveOffer)}
-                        <span
-                          className="rounded-br-3xl rounded-tr-3xl rounded-tl-lg rounded-bl-lg bg-primary border-[1px] cursor-pointer border-white text-white px-6 py-[3px] items-center justify-center
-                                    hover:bg-transparent hover:scale-110 ease-in-out duration-300
-                                            
-                                    xs:text-[10px]
-                                    md:text-[20px]
-                                    lg:text-[10px]
-                                    xl:text-[15px]">
-                          {t(product.discount)}
-                        </span>
-                      </p>
+                  className={`flex items-center m-0 p-0 rounded-xl overflow-hidden
+                              ${
+                                isGrabbing
+                                  ? "cursor-grabbing"
+                                  : "cursor-grab"
+                              }`}>
+                  <div className="flex items-center w-[100%]
+                                  
+                                  xl:h-[700px]">
+                    <div
+                      className="flex flex-col absolute h-auto justify-center
+                                                    
+                                xs:w-[100%] xs:pl-7 xs:bottom-0 xs:mb-[30px]
+                                sm:w-[80%] sm:bottom-0 sm:mb-[30px]
+                                md:w-[80%] md:bottom-0 md:mb-[70px] md:gap-5
+                                lg:w-[90%] lg:mb-[30px] lg:gap-2
+                                xl:w-[90%] xl:pl-12
+                                2xl:bottom-0 2xl:mb-[30px] 2xl:gap-0">
+                      {/* ============= Exclusive offer ============= */}
+                      <div
+                        className="gap-3
+                                                      
+                                  xs:flex">
+                        <p className="flex text-white gap-3 items-center
+                                                                      
+                                      xs:text-[12px]
+                                      sm:text-[13px]
+                                      md:text-[20px]
+                                      lg:text-[15px]
+                                      2xl:text-[16px]">
+                          {t(product.exclusiveOffer)}
+                        </p>
+                        <div
+                          className="w-fit rounded-br-3xl rounded-tr-3xl rounded-tl-lg rounded-bl-lg bg-primary border-[1px] cursor-pointer
+                                    border-white text-white items-center justify-center
+                                      hover:bg-transparent hover:scale-110 ease-in-out duration-300">
+                          <p className="
+                                                                              
+                                      xs:px-4 xs:text-[12px] xs:py-[1px]
+                                      md:text-[20px] md:px-6 md:py-[3px]
+                                      lg:text-[15px]
+                                      2xl:text-[16px]">
+                            {t(product.discount)}
+                          </p>
+                        </div>
+                      </div>
                       <h1 className="text-white font-camptonBold leading-tight my-3
-
-                                      xs:text-[20px]
-                                      sm:text-[35px]
-                                      md:text-[60px]
-                                      lg:text-[29px]
-                                      xl:text-[40px]">
+                                                      
+                                    xs:text-[30px] xs:w-[80%]
+                                    md:text-[65px]
+                                    lg:text-[35px]
+                                    2xl:text-[45px]">
                         {t(product.title)}
                       </h1>
-                      <p
-                        className="text-white font-camptonLigh
-                      
-                                     xs:text-[10px]
-                                     md:text-[25px]
-                                     lg:text-[12px] lg:w-[95%]">
+                      <p className="text-white font-camptonBook
+                                                      
+                                    xs:text-[12px] xs:w-[80%]
+                                    sm:text-[13px] sm:w-[90%]
+                                    md:w-[100%] md:text-[20px]
+                                    lg:text-[15px] lg:w-[90%]
+                                    2xl:text-[16px] 2xl:w-[100%]">
                         {t(product.paragraph)}
                       </p>
                       <div className="w-auto mt-7">
                         <button
-                          className=" flex items-center justify-center gap-3 border-[1px] bg-primary border-white
-                                    text-white px-4 py-2 rounded-br-3xl rounded-tr-3xl rounded-tl-lg rounded-bl-lg
-                                      hover:scale-110 hover:border-[1px] hover:bg-transparent ease-in-out duration-300
-                                      
-                                      xs:gap-2 xs:px-3 xs:py-1
-                                      md:gap-3 md:px-4 md:py-2">
+                          className="flex items-center justify-center bg-primary border-[1px] border-white
+                                  text-white rounded-br-3xl rounded-tr-3xl rounded-tl-lg rounded-bl-lg
+                                    hover:scale-110 hover:border-[1px] hover:bg-transparent ease-in-out duration-300
+                                                                          
+                                    xs:gap-2 xs:px-3 xs:py-1
+                                    md:gap-3 md:px-4 md:py-2">
                           <p className="
-                          
-                                      xs:text-[10px]
-                                      md:text-[25px]
-                                      lg:text-[12px]
+                                     
+                                      xs:text-[12px]
+                                      md:text-[20px]
+                                      lg:text-[15px]
                                       2xl:text-[20px]">
-                            {t(product.addToCart)}</p>
+                            {t(product.addToCart)}
+                          </p>
                           <i>
-                            <IoIosArrowForward />
+                            <IoIosArrowForward
+                              className="
+                                        
+                                        xs:text-[12px]
+                                        md:text-[15px]
+                                        lg:text-[20px]
+                                        2xl:text-[20px]"/>
                           </i>
                         </button>
                       </div>
