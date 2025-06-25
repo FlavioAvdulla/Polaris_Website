dbd8af7c30d8b98c7aa42b4e0a669a7d4c63103f
Flavio Avdulla <a.flavio4366@gmail.com>
2025-06-25 15:00:40 +0200

    web_update

--- gp.sh
+++ gp.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-git add .
-git rm --cached .env
+# Remove .env from Git (keeps local file)
+git rm --cached .env  
+
+# Add remaining files
+git add .  
+
+# Commit & push
 git commit -m "$1"
 git push
